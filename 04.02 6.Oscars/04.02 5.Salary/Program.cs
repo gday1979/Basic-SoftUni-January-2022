139cba09a057eb411b845042a1cd4fbb563ec5a9
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2022-04-16 18:01:20 +0300

    Add files via upload

---
+++ 04.02 6.Oscars/04.02 5.Salary/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _04._02_5.Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+            int facebook = 150;
+            int instagram = 100;
+            int reddit = 50;
+
+            for (int i = 1; i <= n; i++)
+            {
+                string page = Console.ReadLine();
+                if (page == "Facebook")
+                {
+                    salary -= facebook;
+                }
+                else if (page == "Instagram")
+                {
+                    salary -= instagram;
+                }
+                else if (page == "Reddit")
+                {
+                    salary -= reddit;
+                }
+                else
+                {
+                    salary -= 0;
+                }
+            }
+            if (salary <= 0)
+            { Console.WriteLine("You have lost your salary."); }
+
+            else
+
+            { Console.WriteLine($"{salary}"); }
+        }
+    }
+}
---
+++ 04.03 4.Clever Lilly/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _04._03_4.Clever_Lilly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal years = decimal.Parse(Console.ReadLine());
+            decimal washingMachine = decimal.Parse(Console.ReadLine());
+            int toy = int.Parse(Console.ReadLine());
+
+            decimal evenCounter = 0;
+            decimal oddCounter = 0;
+            decimal total = 0;
+
+            for (decimal i = 0; i < years; i++)
+            {
+                oddCounter = years / 2;
+
+                if (years % 2 != 0) { oddCounter++; }
+                evenCounter = years / 2;
+            }
+            int sborNaChislata = 0, j;
+
+            for (j = 0; j <= years; j = j + 2)
+            {
+                sborNaChislata += j;
+            }
+            total = sborNaChislata * 5;
+
+            decimal totalPlus = (int)oddCounter * toy;
+            decimal savedMoney = total + totalPlus - (int)evenCounter;
+
+            if (savedMoney >= washingMachine)
+            {
+                Console.WriteLine("Yes! {0:f2}", savedMoney - washingMachine);
+            }
+            else
+            {
+                Console.WriteLine("No! {0:f2}", Math.Abs(washingMachine - savedMoney));
+
+            }
+        }
+    }
+}
